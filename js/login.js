ceb60d32c9a732f55dbe66519b1e0684a764c7af
Michy <ricotero1309@gmail.com>
2023-11-24 20:20:06 -0300

    Login y Registro Terminados

--- js/login.js
+++ js/login.js
@@ -1,52 +1,65 @@
 const validarUsuario = document.querySelector('#loginForm');
-
 validarUsuario.addEventListener('submit', validarLogin);
 
 let usuarios = JSON.parse(localStorage.getItem('usuarios'));
 
 function validarLogin(e) {
-		e.preventDefault();
+    e.preventDefault();
 
-	const email = document.querySelector('#email').value;
-	const password = document.querySelector('#password').value;
+    const email = document.querySelector('#email').value;
+    const password = document.querySelector('#password').value;
 
-	const validarEmail = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/;
-	const resultadoValidacion = validarEmail.test(email);
+    const validarEmail = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/;
+    const resultadoValidacion = validarEmail.test(email);
 
-	if (!email || !password) {
-		Swal.fire({
-			icon: "error",
-			title: "Oops...",
-			text: "Todos los campos son obligatorios!",
-		  });
-		return;
-	} else if (!resultadoValidacion) {
-		Swal.fire({
-			icon: "error",
-			title: "Oops...",
-			text: "Email no valido!",
-		  });
-		return;
-		}else if (password.length < 7) {
-			Swal.fire({
-				icon: "error",
-				title: "Oops...",
-				text: "La contraseña debe ser mayor a 6 caracteres!",
-			  });
-			  return;
-	}
+    if (!email || !password) {
+        Swal.fire({
+            icon: "error",
+            title: "Oops...",
+            text: "Todos los campos son obligatorios!",
+        });
+        return;
+    } else if (password.length < 7) {
+        Swal.fire({
+            icon: "error",
+            title: "Oops...",
+            text: "La contraseña debe ser mayor a 6 caracteres!",
+        });
+        return;
+    } else if (!resultadoValidacion) {
+        Swal.fire({
+            icon: "error",
+            title: "Oops...",
+            text: "Email no valido!",
+        });
+        return;
+    }
 
-	const existeEmail = usuarios.find(function (usuario) {
-		return usuario.email == email;
-	});
+    const existeEmail = usuarios.find(function (usuario) {
+        return usuario.email == email;
+    });
 
-	if (existeEmail !== undefined) {
-		if (existeEmail.password === password) {
-			if (existeEmail.id == 1698276594011) {
-				location.href = 'admin.html';
-			} else {
-				location.href = 'home.html';
-			}
-		}
-	}
+    if (existeEmail !== undefined) {
+        if (existeEmail.password === password) {
+            if (existeEmail.id == 1698276594011) {
+                // location.href = 'home.html';
+            } else {
+                location.href = '../index.html';
+                const currentUrl = window.location.href;
+                console.log(currentUrl);
+            }
+        } else {
+            Swal.fire({
+                icon: "error",
+                title: "Oops...",
+                text: "Usuario o contraseña Incorrecto!",
+            });
+        }
+    } else {
+        Swal.fire({
+            icon: "error",
+            title: "Oops...",
+            text: "Usuario o contraseña Incorrecto!",
+        });
+    }
 }
