57581531611cac9a89dd4ac5fd823653fadc9633
Ale Bardin <alejandro.bardin01@gmail.com>
2023-11-25 13:25:52 -0300

    avance en comentarios de la pagina

--- js/detallePelicula.js
+++ js/detallePelicula.js
@@ -1,14 +1,49 @@
-var boton = document.querySelectorAll(".boton-tr");
-var mostrarComentario = document.querySelectorAll(".no_mostrar");
 
-boton.forEach(function(elemento, indice){
-    elemento.addEventListener("click", function(){
-        mostrarComentario[indice].classList.toggle("mostrar_video");
-        if (mostrarComentario[indice].classList.contains("mostrar_video")) {
-            elemento.innerHTML = "Ver Menos";
+          
+          function mostrar(){
+            document.getElementById('trailer').style.display ='block';
+          }
 
-        } else {
-            elemento.innerHTML = "Ver Trailer";
-        }
-    })
-})
+          function ocultar(){
+            document.getElementById('trailer').style.display ='none';
+            
+          }
+
+
+                   
+     
+
+        function add3(){
+          let content = `<div>
+          <div class="publicacion-realizada mb-5 mt-5">
+          <div class="usuario-publico">
+              <div class="avatar">
+                  <img src="http://localhost/multimedia/relleno/img-c9.png" alt="img">
+              </div>
+              <div class="contenido-publicacion">
+                  <h4>Carolina de la valle</h4>
+                  <ul>
+                      <li>Hace 3 min</li>
+                  </ul>
+              </div>
+              <div class="menu-comentario">
+                  <i class="fas fa-pen"></i>
+                  <ul class="menu">
+                      <li><a href="">Editar</a></li>
+                      <li><a href="">Eliminar</a></li>
+                  </ul>
+              </div>
+          </div>
+          <p>${product.value}</p>
+          
+          <div class="botones-comentario">
+              <button type="" class="boton-puntuar">
+                  <i class="fas fa-thumbs-up"></i>
+                  45
+              </button>
+              
+          </div>
+      </div>`;
+      
+          list.innerHTML += content;
+      }
