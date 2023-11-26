92fe68ee2e494b9e67e075c4c799559e8cf19269
Oscar <ofvinals@gmail.com>
2023-11-26 12:35:32 -0300

    Funcion Busqueda de peliculas

--- js/main.js
+++ js/main.js
@@ -197,6 +197,52 @@ const slider3 = document.querySelector('.slider3');
 
 let peliculas = JSON.parse(localStorage.getItem('peliculas')) || arrayPeliculas;
 
+/* funcion busqueda */
+const buscarButton = document.getElementById('buscarButton');
+
+    buscarButton.addEventListener('click', buscarPeli);      
+
+function buscarPeli(e){
+	e.preventDefault();
+	
+	const inputBusqueda = document.getElementById('busqueda').value.toLowerCase();
+ 
+	const peliculasFiltradas = arrayPeliculas.filter(pelicula =>
+	pelicula.titulo.toLowerCase().includes(inputBusqueda));
+	
+		const peliculaEncontrada = JSON.stringify(peliculasFiltradas)
+		if(peliculasFiltradas !=""){
+			const titulos = peliculasFiltradas.map(pelicula => pelicula.titulo).join(', ');
+			const imagen = peliculasFiltradas.map(pelicula => pelicula.img).join(', ');
+			const descripcion = peliculasFiltradas.map(pelicula => pelicula.descripcion).join(', ');
+			Swal.fire({
+				title: `${titulos}`,
+				text: `${descripcion}`,
+				imageUrl: `${imagen}`,
+				imageWidth: 400,
+				imageHeight: 200,
+				imageAlt: "imagen pelicula",
+				showCancelButton: true,
+  				confirmButtonColor: "#3085d6",
+  				cancelButtonColor: "#d33",
+  				confirmButtonText: "Ver mas!",
+				cancelButtonText: "Volver",
+			  }).then((result) => {
+				if (result.isConfirmed) {
+				  window.location.href = './pages/error404.html';
+				}
+			  });	  
+			} else	{
+			Swal.fire({
+				icon: "error",
+				title: "Oops...",
+				text: "Pelicula no encontrada!",
+			  });
+			  return;
+		}
+     };
+
+	
 /* carga el slider principal */
 function cargarPeliculas(peliculas) {
 	peliculas.map((pelicula) => {
@@ -258,7 +304,7 @@ function cargarSliders(peliculas) {
 	cardContainers.forEach((item) => {
 		peliculas.map((movie) => {
 			
-			console.log(movie.categoria)
+			//console.log(movie.categoria)
 			if(movie.categoria == "Aventuras"){
 				
 				const card = document.createElement('div');
