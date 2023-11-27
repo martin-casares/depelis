3646691857034d1077d93afc2fcee1e39edcf5e5
Oscar <ofvinals@gmail.com>
2023-11-27 11:12:54 -0300

    agregue funcion busqueda a todos los nav y termine de corregir enlaces

--- js/registro.js
+++ js/registro.js
@@ -1,3 +1,48 @@
+/* funcion busqueda */
+const buscarButton = document.getElementById('buscarButton');
+
+buscarButton.addEventListener('click', buscarPeli);
+
+let peliculastorage = JSON.parse(localStorage.getItem('peliculas'));
+
+function buscarPeli(e) {
+	e.preventDefault();
+
+	const inputBusqueda = document.getElementById('busqueda').value.toLowerCase();
+
+	const peliculasFiltradas = peliculastorage.filter((pelicula) => pelicula.titulo.toLowerCase().includes(inputBusqueda));
+
+	const peliculaEncontrada = JSON.stringify(peliculasFiltradas);
+	if (peliculasFiltradas != '') {
+		const titulos = peliculasFiltradas.map((pelicula) => pelicula.titulo).join(', ');
+		const imagen = peliculasFiltradas.map((pelicula) => pelicula.img).join(', ');
+		const descripcion = peliculasFiltradas.map((pelicula) => pelicula.descripcion).join(', ');
+		Swal.fire({
+			title: `${titulos}`,
+			text: `${descripcion}`,
+			imageUrl: `${imagen}`,
+			imageWidth: 400,
+			imageHeight: 200,
+			imageAlt: 'imagen pelicula',
+			showCancelButton: true,
+			confirmButtonColor: '#3085d6',
+			cancelButtonColor: '#d33',
+			confirmButtonText: 'Ver mas!',
+			cancelButtonText: 'Volver',
+		}).then((result) => {
+			if (result.isConfirmed) {
+				window.location.href = '../pages/error4042.html';
+			}
+		});
+	} else {
+		Swal.fire({
+			icon: 'error',
+			title: 'Oops...',
+			text: 'Pelicula no encontrada!',
+		});
+		return;
+	}
+}
 class Usuario {
 	constructor(id, nombre, email, password) {
 		this.id = id;
@@ -16,7 +61,7 @@ document.addEventListener('DOMContentLoaded', function () {
 	usuarios = JSON.parse(localStorage.getItem('usuarios')) || [];
 });
 
-document.getElementById("validarRegistro").addEventListener("click", validarRegistro);
+document.getElementById('validarRegistro').addEventListener('click', validarRegistro);
 validarUsuario.addEventListener('submit', validarRegistro);
 function validarRegistro(e) {
 	e.preventDefault();
@@ -29,39 +74,34 @@ function validarRegistro(e) {
 
 	const validarEmail = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/;
 	const resultadoValidacion = validarEmail.test(email);
-	if (
-		nombre === '' ||
-		email === '' ||
-		password === '' ||
-		confirmPassword === ''
-	) {
+	if (nombre === '' || email === '' || password === '' || confirmPassword === '') {
 		Swal.fire({
-			icon: "error",
-			title: "Oops...",
-			text: "Todos los campos son obligatorios!",
-		  });
+			icon: 'error',
+			title: 'Oops...',
+			text: 'Todos los campos son obligatorios!',
+		});
 		return;
 	} else if (!resultadoValidacion) {
 		Swal.fire({
-			icon: "error",
-			title: "Oops...",
-			text: "Email no valido!",
-		  });
+			icon: 'error',
+			title: 'Oops...',
+			text: 'Email no valido!',
+		});
 		return;
 	} else if (password.length < 7) {
 		Swal.fire({
-			icon: "error",
-			title: "Oops...",
-			text: "La contraseña debe ser mayor a 6 caracteres!",
-		  });
+			icon: 'error',
+			title: 'Oops...',
+			text: 'La contraseña debe ser mayor a 6 caracteres!',
+		});
 		return;
 	} else if (password !== confirmPassword) {
 		Swal.fire({
-			icon: "error",
-			title: "Oops...",
-			text: "Las contraseñas deben ser iguales!",
-		  });
-		  return;
+			icon: 'error',
+			title: 'Oops...',
+			text: 'Las contraseñas deben ser iguales!',
+		});
+		return;
 	}
 
 	const existeEmail = usuarios.find(function (usuario) {
@@ -89,11 +129,10 @@ function validarRegistro(e) {
 		timer: 1500,
 	}).then(() => {
 		// Después de mostrar el SweetAlert, redirigir al usuario al formulario de inicio de sesión (login.html)
-		
-	window.location.href = 'login.html';
-	
+
+		window.location.href = 'login.html';
+
 		// Restablecer el formulario después de la redirección
 		validarUsuario.reset();
 	});
-}	
-
+}
